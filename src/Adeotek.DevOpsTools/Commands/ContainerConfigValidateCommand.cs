4a32dfe7acacbb581f3075ce99b97b30dc947b4d
George Benjamin-Schonberger <george.benjamin@gmail.com>
2023-10-03 22:20:19 +0300

    [feat:container] config validate command changes

--- src/Adeotek.DevOpsTools/Commands/ContainerConfigValidateCommand.cs
+++ src/Adeotek.DevOpsTools/Commands/ContainerConfigValidateCommand.cs
@@ -32,7 +32,35 @@ internal class ContainerConfigValidateCommand : CommandBase<ContainerConfigSetti
             PrintMessage("`Image` cannot be empty!", _errorColor);
         }
         
-        // TODO: add Volumes/Network/Restart validations
+        if (config.Ports.Count(e => e.Host == 0 || e.Container == 0) > 0)
+        {
+            hasErrors = true;
+            PrintMessage("`Ports[*].Host`/`Ports[*].Container` cannot be 0!", _errorColor);
+        }
+
+        if (config.Volumes.Count(e => string.IsNullOrEmpty(e.Source) || e.Source.Contains(' ')
+            || string.IsNullOrEmpty(e.Destination) || e.Destination.Contains(' ')) > 0)
+        {
+            hasErrors = true;
+            PrintMessage("`Volume[*].Source`/`Volume[*].Destination` cannot be empty, nor contain any spaces!", _errorColor);
+        }
+        
+        if (config.Network is not null 
+            && (string.IsNullOrEmpty(config.Network.Name) 
+                || (config.Network?.Name.Contains(' ') ?? false)))
+        {
+            hasErrors = true;
+            PrintMessage("`Network.Name` cannot be empty, nor contain any spaces!", _errorColor);
+        }
+        
+        if (config.Restart is not null
+            && config.Restart != "on-failure" 
+            && config.Restart != "always"
+            && config.Restart != "unless-stopped")
+        {
+            hasErrors = true;
+            PrintMessage("`Restart` can only have one of the following values: null, 'always', 'on-failure' or 'unless-stopped'!", _errorColor);
+        }
 
         if (hasErrors)
         {
