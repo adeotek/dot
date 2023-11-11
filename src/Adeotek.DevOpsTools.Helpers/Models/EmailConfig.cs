c06b2203c7b1874643c911e54af217d60da4c7a0
George Benjamin-Schonberger <george.benjamin@gmail.com>
2023-11-11 14:16:07 +0200

    [feat:email] helpers library and EmailHelper class added

---
+++ src/Adeotek.DevOpsTools.Helpers/Models/EmailConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Adeotek.DevOpsTools.Helpers.Models;
+
+public class EmailConfig
+{
+    public SmtpConfig SmtpConfig { get; set; } = default!;
+    public string FromAddress { get; set; } = default!;
+    public string? FromLabel { get; set; }
+    public string ToAddress { get; set; } = default!;
+    public string? Subject { get; set; }
+    public string MessageBody { get; set; } = default!;
+    public bool ReadBodyFromFile { get; set; }
+}
