4280cf2e42dd7942cd58bf8d7d0c918621881da5
George Benjamin-Schonberger <george.benjamin@gmail.com>
2025-01-26 00:23:40 +0200

    [feat:net9] .NET 9 support and dependencies upgrades

--- src/Adeotek.Extensions.ConfigFiles/Converters/StringArrayYamlTypeConverter.cs
+++ src/Adeotek.Extensions.ConfigFiles/Converters/StringArrayYamlTypeConverter.cs
@@ -6,37 +6,37 @@ using YamlDotNet.Serialization.Utilities;
 namespace Adeotek.Extensions.ConfigFiles.Converters;
 
 public class StringArrayYamlTypeConverter(
-    IValueSerializer? _valueSerializer,
-    IValueDeserializer? _valueDeserializer)
+    IValueSerializer? valueSerializer,
+    IValueDeserializer? valueDeserializer)
     : IYamlTypeConverter
 {
     public bool Accepts(Type type) => type == typeof(StringArray) || type == typeof(StringArray?);
     
-    public object ReadYaml(IParser parser, Type type)
+    public object ReadYaml(IParser parser, Type type, ObjectDeserializer rootDeserializer)
     {
-        ArgumentNullException.ThrowIfNull(_valueDeserializer, "No value deserializer object provided");
+        ArgumentNullException.ThrowIfNull(valueDeserializer, "No value deserializer object provided");
         
         if (parser.TryConsume<Scalar>(out var scalar))
         {
             return new StringArray(scalar.Value);
         }
-
-        var value = _valueDeserializer.DeserializeValue(parser, typeof(string[]), new SerializerState(), _valueDeserializer);    
+        
+        var value = valueDeserializer.DeserializeValue(parser, typeof(string[]), new SerializerState(), valueDeserializer);    
         return value is string[] standardValue
             ? new StringArray(standardValue)
             : throw new Exception($"Invalid YAML format for node of type {nameof(StringArray)}");
     }
-    
-    public virtual void WriteYaml(IEmitter emitter, object? value, Type type)
+
+    public void WriteYaml(IEmitter emitter, object? value, Type type, ObjectSerializer serializer)
     {
-        ArgumentNullException.ThrowIfNull(_valueSerializer, "No value serializer object provided");
+        ArgumentNullException.ThrowIfNull(valueSerializer, "No value serializer object provided");
         if (value is StringArray stringArray)
         {
-            _valueSerializer.SerializeValue(emitter, stringArray.Value, typeof(string[]));
+            valueSerializer.SerializeValue(emitter, stringArray.Value, typeof(string[]));
         }
         else
         {
-            _valueSerializer.SerializeValue(emitter, value, type);    
+            valueSerializer.SerializeValue(emitter, value, type);    
         }
     }
 }
--- src/Adeotek.Extensions.Containers/Converters/CustomBaseYamlTypeConverter.cs
+++ src/Adeotek.Extensions.Containers/Converters/CustomBaseYamlTypeConverter.cs
@@ -7,31 +7,31 @@ namespace Adeotek.Extensions.Containers.Converters;
 // Unfortunately the API does not provide these in the ReadYaml and WriteYaml methods,
 // so we are forced to set them from the constructor.
 public abstract class CustomBaseYamlTypeConverter<T>(
-    IValueSerializer? _valueSerializer,
-    IValueDeserializer? _valueDeserializer)
+    IValueSerializer? valueSerializer,
+    IValueDeserializer? valueDeserializer)
     : IYamlTypeConverter
     where T : notnull
 {
     public bool Accepts(Type type) => type == typeof(T);
     
-    public virtual object? ReadYaml(IParser parser, Type type)
+    public virtual object? ReadYaml(IParser parser, Type type, ObjectDeserializer rootDeserializer)
     {
-        ArgumentNullException.ThrowIfNull(_valueDeserializer, "No value deserializer object provided");
+        ArgumentNullException.ThrowIfNull(valueDeserializer, "No value deserializer object provided");
         if (TryCustomParse(parser, out var customValue))
         {
             return customValue;
         }
 
-        var value = _valueDeserializer.DeserializeValue(parser, type, new SerializerState(), _valueDeserializer);
+        var value = valueDeserializer.DeserializeValue(parser, type, new SerializerState(), valueDeserializer);
         return value is T standardValue
                ? standardValue
                : throw new Exception($"Invalid YAML format for node of type {typeof(T).Name}");
     }
     
-    public virtual void WriteYaml(IEmitter emitter, object? value, Type type)
+    public virtual void WriteYaml(IEmitter emitter, object? value, Type type, ObjectSerializer serializer)
     {
-        ArgumentNullException.ThrowIfNull(_valueSerializer, "No value serializer object provided");
-        _valueSerializer.SerializeValue(emitter, value, type);
+        ArgumentNullException.ThrowIfNull(valueSerializer, "No value serializer object provided");
+        valueSerializer.SerializeValue(emitter, value, type);
     }
 
     protected virtual bool TryCustomParse(IParser parser, out T? value)
